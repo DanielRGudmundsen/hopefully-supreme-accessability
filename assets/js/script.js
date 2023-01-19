a8663918d7ddc494dbd0977ea013e30e8de9f98a
Daniel Gudmundsen <danielrgudmundsen@outlook.com>
2023-01-18 20:53:53 -0800

    started making the website follow accessability standards -changed the name of title element -added a script.js file -made variables and started making function so it sounds better on screen reader

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//Use javascript to select and recolor 'seo' within Horiseon (h1 element) to be the desired color without impacting the screen readers ability to correctly enunciate the whole word instead of splitting it up and sounding funky
+
+// Selects the h1 element using queryselector and uses that to create a variable called element
+var element = document.queryselector("h1");
+
+// retrieving the text from the element by using innerHTML to denote what is contained within the h1 element
+var horiseon = element.innerHTML;
+
+// using substring to pick out the letters 'Hori' 
+var hori = text.substring(0, 4);
+
+// using substring to pick out the letters 'seo' 
+var seo = text.substring(4,7);
+
+// using substring to pick out the letter 'n'
+var n = text.substring(7);
+
+// use a function to set the innerHTML of h1 to be the sum of the variables hori + seo + n while stylizing the 'seo' to the desired colors
+function colorLetters() {
+    element.innerHTML = hori + "<span style='color:#d9dcd6;'>" + seo + "</span>" + n;
+
+//To do connect to html document run after DOM loads
